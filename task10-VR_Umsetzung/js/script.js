e3b2b0931758c9970ce933096fcdd51412007f51
mnlmrngl <64210587+mnlmrngl@users.noreply.github.com>
2021-01-08 13:33:09 +0100

    added task 10

--- task10-VR_Umsetzung/js/script.js
+++ task10-VR_Umsetzung/js/script.js
@@ -3,9 +3,12 @@ var time;
 
 
 
+
+
 setInterval(function () {
     time = video.currentTime;
-    console.log(time);
+
+    currentTime.setAttribute('value', Math.round(time) + 's / 67s');
 
     if (time >= 20 && time <= 21) {
         document.getElementById('formate_beibehalten').setAttribute('visible', 'true');
@@ -16,13 +19,33 @@ setInterval(function () {
     }
 }, 1000);
 
-function clicked_note(note){
-    var eye = note.firstElementChild;
-    eye.setAttribute('visible', 'false');
+function clicked_note(note) {
+    if (note.id == 'formate_beibehalten') {
+        note.setAttribute('text', 'color: white; align: center; opacity: 1; value: Einzelne Formate beibehalten; color: black; wrapCount: 10');
+        note.setAttribute('material', 'ccolor: #ee8fa8; shader: flat; opacity: 1');
+    } else if (note.id == 'rausch') {
+        note.setAttribute('text', 'color: white; align: center; opacity: 1; value: Ich kann nicht lesen was hier steht; color: black; wrapCount: 10');
+        note.setAttribute('material', 'ccolor: #ee8fa8; shader: flat; opacity: 1');
+    } else if (note.id == 'familie') {
+        note.setAttribute('text', 'color: white; align: center; opacity: 1; value: Zeit für die Familie; color: black; wrapCount: 10');
+        note.setAttribute('material', 'ccolor: #ee8fa8; shader: flat; opacity: 1');
+    }
+
+    var eyes = document.querySelectorAll('.eye');
+    for (i = 0; i < eyes.length; i++) {
+        eyes[i].setAttribute('visible', 'false');
+    }
 }
 
 
-function leave_note(note){
-    var eye = note.firstElementChild;
-    eye.setAttribute('visible', 'true');
+function leave_note(note) {
+    setTimeout(function () {
+        note.setAttribute('text', 'opacity: 0');
+        note.setAttribute('material', 'opacity: 0');
+
+        var eyes = document.querySelectorAll('.eye');
+        for (i = 0; i < eyes.length; i++) {
+            eyes[i].setAttribute('visible', 'true');
+        }
+    }, 550);
 }
